0e5ec73b27543c50bc0c847b6c723ceb7a68ae11
Shyam Chen <shyamchen1994@gmail.com>
2017-07-16 17:40:03 +0800

    8th Commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const About = (props) => (
-  <div>
-    About Component - { props.value }
-  </div>
-);
+import xhr from 'superagent';
+
+const About = ({ value, text }) => {
+
+  text = 'List'
+
+  const getList = () => {
+    xhr.get('https://web-go-demo.herokuapp.com/__/list')
+      .then(res => {
+        console.log(res.text);
+      });
+  }
+
+
+  return (
+    <div>
+      About Component - { value }
+      <button onClick={ getList }>Get List</button>
+      { text }
+    </div>
+  )
+};
 
 About.propTypes = {
-  value: PropTypes.string
+  value: PropTypes.string,
+  text: PropTypes.string,
 }
 
 export default About;
