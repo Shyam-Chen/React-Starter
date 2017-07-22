30e4a9335e71f26af982d9680fe8645f74bd6f1d
Shyam Chen <shyamchen1994@gmail.com>
2017-07-22 16:01:03 +0800

    24th Commit

--- src/components/RESTAdd.js
+++ src/components/RESTAdd.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Button, Input } from 'semantic-ui-react';
 
-export const RESTAdd = ({ actions, value, valueActions }) => {
-  const onAdd = () => {
-    if (value.addValue.foo && value.addValue.bar) {
-      actions.onAddItem(value.addValue.foo, value.addValue.bar);
-      value.addValue.foo = '';
-      value.addValue.bar = '';
-    }
-  };
+export class RESTAdd extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <Input value={ value.addValue.foo } onChange={ event => valueActions.onChangeAddFoo(event.target.value) } />
-      { ' - ' }
-      <Input value={ value.addValue.bar } onChange={ event => valueActions.onChangeAddBar(event.target.value) } />
-      { ' ' }
-      <Button basic color="black" onClick={ onAdd }>Add</Button>
-    </div>
-  );
-};
+    this.state = {
+      foo: '',
+      bar: ''
+    };
+
+    this.onAdd = this.onAdd.bind(this);
+  }
+
+  onAdd() {
+    const { foo, bar } = this.state;
+
+    if (foo && bar) {
+      this.props.actions.onAddItem(foo, bar);
+      this.setState({ foo: '', bar: '' });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Input value={ this.state.foo } onChange={ event => this.setState({ foo: event.target.value }) } />
+        { ' - ' }
+        <Input value={ this.state.bar } onChange={ event => this.setState({ bar: event.target.value }) } />
+        { ' ' }
+        <Button basic color="black" onClick={ this.onAdd }>Add</Button>
+      </div>
+    );
+  }
+}
+
+// -
+
+// import React from 'react';
+//
+// import { Button, Input } from 'semantic-ui-react';
+//
+// export const RESTAdd = ({ actions, value, valueActions }) => {
+//   const onAdd = () => {
+//     if (value.addValue.foo && value.addValue.bar) {
+//       actions.onAddItem(value.addValue.foo, value.addValue.bar);
+//       value.addValue.foo = '';
+//       value.addValue.bar = '';
+//     }
+//   };
+//
+//   return (
+//     <div>
+//       <Input value={ value.addValue.foo } onChange={ event => valueActions.onChangeAddFoo(event.target.value) } />
+//       { ' - ' }
+//       <Input value={ value.addValue.bar } onChange={ event => valueActions.onChangeAddBar(event.target.value) } />
+//       { ' ' }
+//       <Button basic color="black" onClick={ onAdd }>Add</Button>
+//     </div>
+//   );
+// };
