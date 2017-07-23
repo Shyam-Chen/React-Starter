eac3daaa666ea288c61ff46f8235f038fb53a2ed
Shyam Chen <shyamchen1994@gmail.com>
2017-07-23 21:02:09 +0800

    28th Commit

--- src/actions/rest.js
+++ src/actions/rest.js
@@ -2,12 +2,31 @@ import axios from 'axios';
 
 import { CREATE, READ, UPDATE, DELETE } from '../constants';
 
-export const onCreate = () => ({ type: CREATE });
+const url = 'https://web-go-demo.herokuapp.com/__/list';
+
+export const onCreate = (data) => ({ type: CREATE, data });
 export const onRead = (data) => ({ type: READ, data });
-export const onUpdate = () => ({ type: UPDATE });
-export const onDelete = () => ({ type: DELETE });
+export const onUpdate = (data) => ({ type: UPDATE, data });
+export const onDelete = (data) => ({ type: DELETE, data });
 
 export const onSearch = () =>
   dispatch =>
-    axios.get('https://web-go-demo.herokuapp.com/__/list')
+    axios.get(url)
       .then(response => dispatch(onRead(response.data)));
+
+// axios.get(`${url}?text=${text}`)
+
+export const onAdd = (text) =>
+  dispatch =>
+    axios.post(url, { text })
+      .then(response => dispatch(onCreate(response.data)));
+
+export const onSave = (id, text) =>
+  dispatch =>
+    axios.put(`${url}/${id}`, { text })
+      .then(response => dispatch(onUpdate(response.data)));
+
+export const onRemove = (id) =>
+  dispatch =>
+    axios.delete(`${url}/${id}`)
+      .then(response => dispatch(onDelete(response.data)));
--- src/containers/REST.js
+++ src/containers/REST.js
@@ -6,21 +6,23 @@ import { Button } from 'semantic-ui-react';
 
 import * as actions from '../actions/rest';
 
-export const REST = ({ rest, actions }) => {
-  return (
-    <div>
-      <Button basic color="black" onClick={ actions.onSearch }>Search</Button>
+export const REST = ({ rest, actions }) => (
+  <div>
+    <Button basic color="black" onClick={ actions.onSearch }>Search</Button>
 
-      <ul>
-        {
-          rest.map((item) => (
-            <li key={ item._id }>{ item.text }</li>
-          ))
-        }
-      </ul>
-    </div>
-  );
-};
+    <ul>
+      {
+        rest.map((item) => (
+          <li key={ item._id }>
+            { item.text }
+
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+);
+
 
 export default connect(
   // map state to props
