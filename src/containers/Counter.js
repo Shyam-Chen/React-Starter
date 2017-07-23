aa07def41447cec3aa922eb937bc9d589fe87ddb
Shyam Chen <shyamchen1994@gmail.com>
2017-07-23 19:19:29 +0800

    27th Commit

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { Button } from 'semantic-ui-react';
+
 import * as actions from '../actions/counter';
 
 const Counter = ({ counter, actions }) => (
   <div>
-    Clicked: { counter.value } times
-    <button onClick={ actions.onIncrement }>+</button>
-    <button onClick={ actions.onDecrement }>-</button>
-    <button onClick={ actions.onIncrementAsync }>+ (Async)</button>
-    <button onClick={ actions.onIncrementIfOdd }>+ (If Odd)</button>
+    Clicked: { counter.value } times { ' ' }
+    <Button onClick={ actions.onIncrement }>+</Button>
+    <Button onClick={ actions.onDecrement }>-</Button>
+    <Button onClick={ actions.onIncrementAsync }>+ (Async)</Button>
+    <Button onClick={ actions.onIncrementIfOdd }>+ (If Odd)</Button>
   </div>
 );
 
