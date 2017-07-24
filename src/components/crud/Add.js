3106e8d30450068ad2042a9f7a0d0070f5cafcf1
Shyam Chen <shyamchen1994@gmail.com>
2017-07-24 14:20:33 +0800

    32nd Commit

--- src/components/crud/Add.js
+++ src/components/crud/Add.js
@@ -1,37 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Button, Input } from 'semantic-ui-react';
 
-export class Add extends Component {
-  constructor(props) {
-    super(props);
+export const Add = ({ actions }) => {
+  let primary, accent;
 
-    this.state = {
-      primary: '',
-      accent: ''
-    };
+  const onAdd = (event) => {
+    event.preventDefault();
 
-    this.onAdd = this.onAdd.bind(this);
-  }
-
-  onAdd() {
-    const { primary, accent } = this.state;
-
-    if (primary && accent) {
-      this.props.actions.onAddItem(primary, accent);
-      this.setState({ primary: '', accent: '' });
+    if (primary.inputRef.value && accent.inputRef.value) {
+      actions.onAddItem(primary.inputRef.value, accent.inputRef.value);
+      primary.inputRef.value = '';
+      accent.inputRef.value = '';
     }
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Input value={ this.state.primary } onChange={ event => this.setState({ primary: event.target.value }) } />
+  return (
+    <div>
+      <form onSubmit={ onAdd }>
+        <Input ref={ node => primary = node } />
         { ' - ' }
-        <Input value={ this.state.accent } onChange={ event => this.setState({ accent: event.target.value }) } />
+        <Input ref={ node => accent = node } />
         { ' ' }
-        <Button basic color="black" onClick={ this.onAdd }>Add</Button>
-      </div>
-    );
-  }
+        <Button basic color="black" type="submit">Add</Button>
+      </form>
+    </div>
+  );
 }
+
+// import React, { Component } from 'react';
+//
+// import { Button, Input } from 'semantic-ui-react';
+//
+// export class Add extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       primary: '',
+//       accent: ''
+//     };
+//
+//     this.onAdd = this.onAdd.bind(this);
+//   }
+//
+//   onAdd() {
+//     const { primary, accent } = this.state;
+//
+//     if (primary && accent) {
+//       this.props.actions.onAddItem(primary, accent);
+//       this.setState({ primary: '', accent: '' });
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <Input value={ this.state.primary } onChange={ event => this.setState({ primary: event.target.value }) } />
+//         { ' - ' }
+//         <Input value={ this.state.accent } onChange={ event => this.setState({ accent: event.target.value }) } />
+//         { ' ' }
+//         <Button basic color="black" onClick={ this.onAdd }>Add</Button>
+//       </div>
+//     );
+//   }
+// }
