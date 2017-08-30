1a1b2e71531626c2341ed35847a5e89e2a22908e
Shyam Chen <shyamchen1994@gmail.com>
2017-08-30 16:08:41 +0800

    68th Commit

--- src/app/counter/reducer.js
+++ src/app/counter/reducer.js
@@ -1,14 +1,12 @@
+import { handleActions } from 'redux-actions';
+
 import { INITIAL, INCREMENT, DECREMENT } from './constants';
 
-export default (state = INITIAL, action) => {
-  const { type } = action;
-
-  switch (type) {
-    case INCREMENT:
-      return { ...state, value: state.value + 1 };
-    case DECREMENT:
-      return { ...state, value: state.value - 1 };
-    default:
-      return state;
+export default handleActions({
+  [INCREMENT](state) {
+    return { ...state, value: state.value + 1 };
+  },
+  [DECREMENT](state) {
+    return { ...state, value: state.value - 1 };
   }
-};
+}, INITIAL);
