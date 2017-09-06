0db5fc3cae901991dddd0ba257d9b8300b25e32f
Shyam Chen <shyamchen1994@gmail.com>
2017-09-06 15:39:00 +0800

    81st Commit

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -5,7 +5,7 @@ import { Route } from 'react-router';
 import App from './App';
 import { Counter } from './counter';
 import { CRUD } from './crud';
-import { REST } from './REST';
+import { REST } from './rest';
 import { GraphQL } from './graphql';
 
 const Routes = () => (
