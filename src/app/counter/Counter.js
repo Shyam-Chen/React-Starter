ed24033cd1f400eeb08eb70e213370534b6859cf
Shyam Chen <shyamchen1994@gmail.com>
2017-09-28 13:27:58 +0800

    124th Commit

--- src/app/counter/Counter.js
+++ src/app/counter/Counter.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { Typography, Button } from 'material-ui';
 
 import Navigation from '~/shared/Navigation';
-import RaisedButton from '~/shared/RaisedButton';
+import TealRaisedButton from '~/shared/TealRaisedButton';
 
 import * as actions from './actions';
 
@@ -12,21 +12,40 @@ const Counter = ({ counter, actions }) => (
   <div className="container">
     <Navigation />
 
-    <Typography type="headline" component="h3">
-      Clicked: {counter.value} times, value is {counter.value % 2 === 0 ? 'even' : 'odd'}.
-    </Typography>
-    <Typography type="body1" component="p">
-      <Button raised color="primary" onClick={actions.increment}>Increment</Button> {' '}
-      <Button raised color="primary" onClick={actions.decrement}>Decrement</Button> {' '}
-      <Button raised color="accent" onClick={actions.incrementAsync}>Increment (Async)</Button> {' '}
-      <Button raised color="accent" onClick={actions.decrementAsync}>Decrement (Async)</Button> {' '}
-      <RaisedButton text="Increment (If Odd)" onClick={actions.incrementIfOdd} /> {' '}
-      <RaisedButton text="Increment (If Even)" onClick={actions.incrementIfEven} /> {' '}
-    </Typography>
+    <div className="typography">
+      <Typography type="headline" component="h3">
+        Clicked: {counter.value} times, value is {counter.value % 2 === 0 ? 'even' : 'odd'}.
+      </Typography>
+    </div>
+
+    <div className="typography">
+      <Typography type="body1" component="p">
+        <Button raised color="primary" onClick={actions.increment}>Increment</Button> {' '}
+        <Button raised color="primary" onClick={actions.decrement}>Decrement</Button>
+      </Typography>
+    </div>
+
+    <div className="typography">
+      <Typography type="body1" component="p">
+        <Button raised color="accent" onClick={actions.incrementAsync}>Increment (Async)</Button> {' '}
+        <Button raised color="accent" onClick={actions.decrementAsync}>Decrement (Async)</Button>
+      </Typography>
+    </div>
+
+    <div className="typography">
+      <Typography type="body1" component="p">
+        <TealRaisedButton text="Increment (If Odd)" onClick={actions.incrementIfOdd} /> {' '}
+        <TealRaisedButton text="Increment (If Even)" onClick={actions.incrementIfEven} />
+      </Typography>
+    </div>
 
     <style jsx>{`
       .container {
         padding: 1rem;
+      }
+
+      .typography {
+        padding: .25rem 0;
       }
     `}</style>
   </div>
