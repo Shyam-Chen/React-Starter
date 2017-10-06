94cf86262a794049419aa24b69caf35893ae4013
Shyam Chen <shyamchen1994@gmail.com>
2017-10-06 19:33:07 +0800

    156th Commit

--- src/app/shared/Navigation.js
+++ src/app/shared/Navigation.js
@@ -7,7 +7,7 @@ import { AppBar, Toolbar, IconButton, Icon, Typography } from 'material-ui';
 
 import VariantButton from '~/shared/VariantButton';
 
-const Navigation = ({ router, actions }) => {
+const Navigation = (/* { router, actions } */) => {
   const inlineStyles = {
     link: { textDecoration: 'none' }
   };
@@ -17,18 +17,21 @@ const Navigation = ({ router, actions }) => {
       <AppBar>
         <Toolbar>
           {
-            router.location.pathname === '/'
+            // router.location.pathname === '/'
+            location.pathname === '/'
               ? <IconButton color="contrast" aria-label="Menu">
                   <Icon>toys</Icon>
                 </IconButton>
-              : <Link to="/" onClick={() => actions.push('/')} style={inlineStyles.link}>
+              : // <Link to="" onClick={() => actions.goBack()} style={inlineStyles.link}>
+                <Link to="" onClick={() => history.back()} style={inlineStyles.link}>
                   <IconButton color="contrast" >
                     <Icon>arrow_back</Icon>
                   </IconButton>
                 </Link>
           }
           <Typography type="title" color="inherit">
-            React by Example, location: <span className="location">{router.location.pathname}</span>.
+            {/* React by Example, location: <span className="location">{router.location.pathname}</span>. */}
+            React by Example, location: <span className="location">{location.pathname}</span>.
           </Typography>
         </Toolbar>
       </AppBar>
@@ -36,37 +39,43 @@ const Navigation = ({ router, actions }) => {
       <div className="filling"></div>
 
       {
-        router.location.pathname === '/'
+        // router.location.pathname === '/'
+        location.pathname === '/'
           ? <div className="buttons">
-              <Link to="/counter" onClick={() => actions.push('/counter')} style={inlineStyles.link}>
+              {/* <Link to="/counter" onClick={() => actions.push('/counter')} style={inlineStyles.link}> */}
+              <Link to="/counter" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="Counter" />
               </Link>
               {'　'}
-              <Link to="/crud" onClick={() => actions.push('/crud')} style={inlineStyles.link}>
+              {/* <Link to="/crud" onClick={() => actions.push('/crud')} style={inlineStyles.link}> */}
+              <Link to="/crud" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="CRUD" />
               </Link>
               {'　'}
-              <Link to="/rest" onClick={() => actions.push('/rest')} style={inlineStyles.link}>
+              {/* <Link to="/rest" onClick={() => actions.push('/rest')} style={inlineStyles.link}> */}
+              <Link to="/rest" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="REST" />
               </Link>
               {'　'}
-              <Link to="/graphql" onClick={() => actions.push('/graphql')} style={inlineStyles.link}>
+              {/* <Link to="/graphql" onClick={() => actions.push('/graphql')} style={inlineStyles.link}> */}
+              <Link to="/graphql" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="GraphQL" />
               </Link>
               {'　'}
-              <Link to="/form-controls" onClick={() => actions.push('/form-controls')} style={inlineStyles.link}>
+              {/* <Link to="/form-controls" onClick={() => actions.push('/form-controls')} style={inlineStyles.link}> */}
+              <Link to="/form-controls" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="Form Controls" />
               </Link>
               {'　'}
-              <Link to="/data-table" onClick={() => actions.push('/data-table')} style={inlineStyles.link}>
+              {/* <Link to="/data-table" onClick={() => actions.push('/data-table')} style={inlineStyles.link}> */}
+              <Link to="/data-table" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="Data Table" />
               </Link>
               {'　'}
-              <Link to="/authorization" onClick={() => actions.push('/authorization')} style={inlineStyles.link}>
+              {/* <Link to="/authorization" onClick={() => actions.push('/authorization')} style={inlineStyles.link}> */}
+              <Link to="/authorization" style={inlineStyles.link}>
                 <VariantButton raised variant="teal" text="Authorization" />
               </Link>
-
-              <div>BUG: actions.goBack() can not return directly to the previous page</div>
             </div>
           : void 0
       }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
@@ -11,11 +12,11 @@ const initialState = window.__INITIAL_STATE__;
 const history = createBrowserHistory();
 const store = configureStore(history, initialState);
 
-let render;
+// let render;
 // if (process.env.NODE_ENV === 'production') {
 //   render = require('react-snapshot').render;
 // } else {
-  render = require('react-dom').render;
+//   render = require('react-dom').render;
 // }
 
 render(
