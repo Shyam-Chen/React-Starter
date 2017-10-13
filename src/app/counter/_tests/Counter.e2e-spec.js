98df9e09729cb64fdf3a152a6ceb20d4c5c50b1c
Shyam-Chen <shyamchen1994@gmail.com>
2017-10-13 16:31:12 +0800

    168th Commit

--- src/app/counter/_tests/Counter.e2e-spec.js
+++ src/app/counter/_tests/Counter.e2e-spec.js
@@ -1,5 +1,10 @@
 describe('Counter', () => {
-  it('', () => {
+  beforeEach(async () => {
+    return await browser.driver.get(`${browser.baseUrl}/counter`);
+  });
 
+  it('should display count', () => {
+    const el = browser.driver.findElement(by.css('.typography > h3'));
+    expect(el.getText()).toEqual('Clicked: 0 times, value is even.');
   });
 });
