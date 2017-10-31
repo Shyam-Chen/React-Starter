91c086f24289a79b59c4ba9611bd2ac6e6bd27da
Shyam-Chen <shyamchen1994@gmail.com>
2017-10-31 16:11:51 +0800

    213th Commit

--- src/app/authorization/Authorization.js
+++ src/app/authorization/Authorization.js
@@ -14,7 +14,7 @@ const Authorization = () => {
     <div>
       <Navigation />
 
-      <div>TODO: API Designing (REST & GraphQL)</div>
+      <div>TODO: API Authentication</div>
 
       <div>
         <TextField
@@ -23,48 +23,70 @@ const Authorization = () => {
           // value={value}
           // onChange={event => actions.setData({ })}
         />
-
+        {' '}
         <TextField
           label="Password"
           margin="normal"
           // value={value}
           // onChange={event => actions.setData({ })}
         />
-
+        {' '}
         <Button
           raised
-          color="blue"
+          color="purple"
           // onClick={async () => {
           //   await actions.setData({ });
           // }}
         >
-          Sign up
+          Sign up (REST)
+        </Button>
+        {' '}
+        <Button
+          raised
+          color="purple"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          Sign up (GraphQL)
         </Button>
       </div>
 
-      <TextField
-        label="Email"
-        margin="normal"
-        // value={value}
-        // onChange={event => actions.setData({ })}
-      />
-
-      <TextField
-        label="Password"
-        margin="normal"
-        // value={value}
-        // onChange={event => actions.setData({ })}
-      />
-
-      <Button
-        raised
-        color="blue"
-        // onClick={async () => {
-        //   await actions.setData({ });
-        // }}
-      >
-        Log in
-      </Button>
+      <div>
+        <TextField
+          label="Email"
+          margin="normal"
+          // value={value}
+          // onChange={event => actions.setData({ })}
+        />
+        {' '}
+        <TextField
+          label="Password"
+          margin="normal"
+          // value={value}
+          // onChange={event => actions.setData({ })}
+        />
+        {' '}
+        <Button
+          raised
+          color="blueGrey"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          Log in (REST)
+        </Button>
+        {' '}
+        <Button
+          raised
+          color="blueGrey"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          Log in (GraphQL)
+        </Button>
+      </div>
 
       <div>
         <Button
@@ -75,7 +97,18 @@ const Authorization = () => {
           // }}
         >
           <i className="fa fa-google" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
-          Sign in with Google
+          Sign in with Google (REST)
+        </Button>
+        {' '}
+        <Button
+          raised
+          color="red"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          <i className="fa fa-google" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
+          Sign in with Google (GraphQL)
         </Button>
       </div>
 
@@ -88,7 +121,18 @@ const Authorization = () => {
           // }}
         >
           <i className="fa fa-facebook-official" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
-          Sign in with Facebook
+          Sign in with Facebook (REST)
+        </Button>
+        {' '}
+        <Button
+          raised
+          color="indigo"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          <i className="fa fa-facebook-official" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
+          Sign in with Facebook (GraphQL)
         </Button>
       </div>
 
@@ -101,7 +145,18 @@ const Authorization = () => {
           // }}
         >
           <i className="fa fa-twitter" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
-          Sign in with Twitter
+          Sign in with Twitter (REST)
+        </Button>
+        {' '}
+        <Button
+          raised
+          color="blue"
+          // onClick={async () => {
+          //   await actions.setData({ });
+          // }}
+        >
+          <i className="fa fa-twitter" aria-hidden="true" style={{ marginRight: '1rem' }}></i>
+          Sign in with Twitter (GraphQL)
         </Button>
       </div>
     </div>
