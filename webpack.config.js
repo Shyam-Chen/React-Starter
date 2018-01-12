67bd1e16e4c498bef14bb336831249b78e30e6f4
Shyam-Chen <shyamchen1994@gmail.com>
2018-01-12 15:43:58 +0800

    263rd Commit

--- webpack.config.js
+++ webpack.config.js
@@ -20,73 +20,13 @@ module.exports = ({ prod = false } = {}) => ({
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              babelrc: false,
-              presets: [
-                [
-                  'env', {
-                    'targets': {
-                      'browsers': ['last 2 versions']
-                    }
-                  }
-                ],
-                'react',
-                'flow'
-              ],
-              plugins: [
-                'transform-runtime',
-                [
-                  'styled-jsx/babel', {
-                    plugins: [
-                      "styled-jsx-plugin-postcss"
-                    ]
-                  }
-                ],
-                'transform-class-properties',
-                'transform-function-bind',
-                'transform-object-rest-spread',
-                [
-                  'babel-plugin-root-import', [
-                    {
-                      rootPathPrefix: '~',
-                      rootPathSuffix: 'src/app'
-                    }
-                  ]
-                ],
-                [
-                  'transform-imports', {
-                    'material-ui': {
-                      transform: 'material-ui/${member}',
-                      preventFullImport: true
-                    },
-                    'rxjs': {
-                      transform: 'rxjs/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    },
-                    'rxjs/observable': {
-                      transform: 'rxjs/observable/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    },
-                    'rxjs/operator': {
-                      transform: 'rxjs/operator/${member}',
-                      preventFullImport: true,
-                      skipDefaultConversion: true
-                    }
-                  }
-                ]
-              ]
-            }
-          }
-        ]
-      }, {
+        use: 'babel-loader'
+      },
+      {
         test: /\.(jpg|png|gif)$/,
         use: 'file-loader'
-      }, {
+      },
+      {
         test: /\.(eot|woff2?|svg|ttf)$/,
         use: 'file-loader'
       }
