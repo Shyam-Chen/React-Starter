3e6326e5555cd11a39b62dbc197f8f453045a39c
Shyam-Chen <shyamchen1994@gmail.com>
2018-02-13 16:53:39 +0800

    283rd Commit

--- src/app/utils/auto-bind.spec.js
+++ src/app/utils/auto-bind.spec.js
@@ -1,7 +1,19 @@
-// import { bindSelectCreators } from './auto-bind';
+import { createSelector } from 'reselect';
+
+import { bindSelectCreators } from './auto-bind';
 
 describe('Auto Bind', () => {
-  it('TODO', () => {
-    expect(true).toBe(true);
+  it('should handle bindSelectCreators', async () => {
+    const state = { value: 10 };
+
+    const evenOrOdd = createSelector(
+      num => num,
+      ({ value }) => value % 2 === 0 ? 'even' : 'odd'
+    );
+
+    const selectors = { evenOrOdd };
+
+    const result = await bindSelectCreators(selectors, state);
+    expect(result.evenOrOdd).toEqual('even');
   })
 });
