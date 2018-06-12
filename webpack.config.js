4546b49090d7819e0bab5bc985a6b63451a4cdec
Shyam Chen <shyamchen1994@gmail.com>
2018-06-12 20:10:57 +0800

    307th Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-const { join, posix } = require('path');
+// const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+// const SWPrecachePlugin = require('sw-precache-webpack-plugin');
+// const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
+// const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const envify = require('process-envify');
+// const uglify = require('uglify-es');
 
-const API_URL = process.env.API_URL || 'https://web-go-demo.herokuapp.com';
+const env = require('./env');
+// const pkg = require('./package');
 
-const SOURCE_ROOT = join(__dirname, 'src');
-const DIST_ROOT = join(__dirname, 'build');
+const SOURCE_ROOT = path.join(__dirname, 'src');
+const DIST_ROOT = path.join(__dirname, 'public');
 
 module.exports = ({ prod = false } = {}) => ({
+  mode: prod ? 'production' : 'development',
   context: SOURCE_ROOT,
   entry: {
     client: './client.js',
@@ -25,34 +33,38 @@ module.exports = ({ prod = false } = {}) => ({
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
-      }, {
+        loader: 'babel-loader',
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader',
+          { loader: 'postcss-loader', options: { sourceMap: true } },
         ],
-      }, {
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: posix.join('assets', 'images/[name].[hash].[ext]'),
+          name: path.posix.join('assets', 'images/[name].[hash].[ext]'),
         },
-      }, {
+      },
+      {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: posix.join('assets', 'medias/[name].[hash].[ext]'),
+          name: path.posix.join('assets', 'medias/[name].[hash].[ext]'),
         },
-      }, {
+      },
+      {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: posix.join('assets', 'fonts/[name].[hash].[ext]'),
+          name: path.posix.join('assets', 'fonts/[name].[hash].[ext]'),
         },
       },
     ].filter(Boolean),
@@ -60,79 +72,81 @@ module.exports = ({ prod = false } = {}) => ({
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      '~': join(SOURCE_ROOT, 'app'),
+      '~': path.join(SOURCE_ROOT, 'app'),
+      '~assets': path.join(SOURCE_ROOT, 'assets'),
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
+    new HtmlPlugin({
       template: 'index.html',
-      inject: true,
       minify: prod && {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true,
       },
       chunksSortMode: prod ? 'dependency' : 'auto',
+      // serviceWorkerLoader: prod
+      //   ? `<script defer>${uglify.minify(fs.readFileSync(path.join(__dirname, './tools/service-worker.prod.js'), 'utf-8')).code}</script>`
+      //   : `<script defer>${fs.readFileSync(path.join(__dirname, './tools/service-worker.dev.js'), 'utf-8')}</script>`,
     }),
-    new CopyWebpackPlugin([
-      'assets/images/favicon.ico',
-    ]),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(prod ? 'production' : 'development'),
-        API_URL: JSON.stringify(API_URL),
+    new ScriptExtHtmlPlugin({
+      defaultAttribute: 'defer',
+      preload: {
+        test: /\.js$/,
+        chunks: 'initial',
+      },
+      prefetch: {
+        test: /\.js$/,
+        chunks: 'all',
       },
     }),
-    prod && new UglifyJSPlugin({
-      uglifyOptions: {
-        compress: {
-          warnings: false,
+    new CopyPlugin([
+      {
+        from: 'assets/**/*',
+        to: DIST_ROOT,
+        ignore: ['assets/styles/**/*'],
+      },
+    ]),
+    new webpack.DefinePlugin(envify(env)),
+    !prod && new webpack.HotModuleReplacementPlugin(),
+    // prod && new SWPrecachePlugin({
+    //   cacheId: pkg.name,
+    //   filename: 'service-worker.js',
+    //   minify: true,
+    //   navigateFallback: 'index.html',
+    //   navigateFallbackWhitelist: [/^(?!\/__).*/],
+    //   staticFileGlobs: [`${path.basename(DIST_ROOT)}/*`],
+    //   stripPrefix: `${path.basename(DIST_ROOT)}/`,
+    // }),
+    // prod && new RobotstxtPlugin(),
+    // prod && new SitemapPlugin(env.SITE_URL, [{ path: '/' }]),
+  ].filter(Boolean),
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          name: 'common',
+          chunks: 'initial',
+          minChunks: 2,
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
         },
       },
-      sourceMap: true,
-      parallel: true,
-    }),
-    prod && new webpack.HashedModuleIdsPlugin(),
-    prod && new webpack.optimize.ModuleConcatenationPlugin(),
-    prod && new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks(module) {
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(join(__dirname, 'node_modules')) === 0
-        );
-      },
-    }),
-    prod && new webpack.optimize.CommonsChunkPlugin({
+    },
+    runtimeChunk: {
       name: 'manifest',
-      minChunks: Infinity,
-    }),
-    prod && new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3,
-    }),
-    !prod && new webpack.HotModuleReplacementPlugin(),
-    !prod && new webpack.NamedModulesPlugin(),
-    !prod && new webpack.NoEmitOnErrorsPlugin(),
-  ].filter(Boolean),
+    },
+  },
   devServer: {
     contentBase: DIST_ROOT,
     historyApiFallback: true,
+    host: env.HOST_NAME,
     hot: true,
     inline: true,
-    port: 8000,
+    port: env.SITE_PORT,
   },
   devtool: prod ? 'hidden-source-map' : 'cheap-module-eval-source-map',
-  node: {
-    setImmediate: false,
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
-  },
 });
