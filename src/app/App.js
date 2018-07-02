4513fe5c4686ea5edcc7da6affaa9cfc82ba2120
Shyam Chen <shyamchen1994@gmail.com>
2018-07-02 17:23:42 +0800

    334th Commit

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Navigation from '~/shared/Navigation';
 import Routes from '~/core/Router';
@@ -11,17 +12,17 @@ import Routes from '~/core/Router';
 import * as actions from './actions';
 
 const App = ({ history }): React$Element<*> => (
-  <div id="app">
-    <ConnectedRouter history={history}>
-      <div>
-        <Navigation />
-        <Routes />
-      </div>
-    </ConnectedRouter>
+  <div>
+    <React.Fragment>
+      <CssBaseline />
 
-    {/* <style jsx>{`
-      #app {}
-    `}</style> */}
+      <ConnectedRouter history={history}>
+        <div>
+          <Navigation />
+          <Routes />
+        </div>
+      </ConnectedRouter>
+    </React.Fragment>
   </div>
 );
 
