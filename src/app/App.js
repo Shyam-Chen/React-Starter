9a96251681e44084265d80aa504c152d583a3ed9
Shyam Chen <shyamchen1994@gmail.com>
2018-07-27 14:04:21 +0800

    359th Commit

--- src/app/App.js
+++ src/app/App.js
@@ -6,22 +6,35 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import pink from '@material-ui/core/colors/pink';
 
 import Layout from '~/shared/Layout';
 import Routes from '~/core/Router';
 
 import * as actions from './actions';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: blue[500] },
+    secondary: { main: pink[500] },
+    type: 'light',
+  },
+});
+
 const App = ({ history }): React$Element<*> => (
   <div>
     <React.Fragment>
       <CssBaseline />
 
-      <ConnectedRouter history={history}>
-        <Layout>
-          <Routes />
-        </Layout>
-      </ConnectedRouter>
+      <MuiThemeProvider theme={theme}>
+        <ConnectedRouter history={history}>
+          <Layout>
+            <Routes />
+          </Layout>
+        </ConnectedRouter>
+      </MuiThemeProvider>
     </React.Fragment>
   </div>
 );
